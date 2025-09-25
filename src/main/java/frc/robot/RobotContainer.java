26b1d64c953e3d7b36525db67e7305394d70e947
AgentKosticka <xlalipop22@seznam.cz>
2025-09-25 12:57:02 +0200

    Prototype - YAGCL support (IMU-less)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,20 +1,17 @@
 package frc.robot;
 
-import com.revrobotics.spark.SparkMax;
-
+import java.io.File;
 import edu.wpi.first.wpilibj.XboxController;
 import swervelib.SwerveDrive;
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveParser;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class RobotContainer {
     private static RobotContainer instance = null;
 
     private final XboxController controller = new XboxController(0);
-    private final double maxSpeed = 0.4;
-    private final double maxRot = 0.4;
-    private final double deadZone = 0.25;
-    
-    //private final SwerveDrive swerve;
-    private SparkMax[] motors = new SparkMax[8];
+    private SwerveDrive swerveDrive;
     
     public RobotContainer() {
         if (instance != null) {
@@ -22,34 +19,29 @@ public class RobotContainer {
         }
         instance = this;
 
-        for (int i = 0; i < 8; i++) {
-            motors[i] = new SparkMax(i + 1, SparkMax.MotorType.kBrushless);
+        // Specify the directory containing your JSON configuration files
+        File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
+
+        // Initialize the swerve drive using the configuration files
+        swerveDrive = null;
+
+        try{
+            swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(10);
+        } catch (Exception e) {
+            System.out.println("Error initializing SwerveDrive: " + e.getMessage());
         }
+
     }
     public void processManualInput() {
         double LeftX = controller.getLeftX();
         double LeftY = controller.getLeftY();
 
-        if (Math.abs(LeftY) < deadZone) {
-            LeftY = 0;
-        }
-        if (Math.abs(LeftX) < deadZone) {
-            LeftX = 0;
-        }
+        double rot = controller.getRightX();
 
-        for (int i = 0; i < 4; i++) {
-            motors[i].set(-LeftY * maxSpeed); // Forward/backward => Forward is negative
-        }
-        for (int i = 4; i < 8; i++) {
-            motors[i].set(-LeftX * maxRot); // Left/right => Right is positive but wheels reverse the movement
-        }
+        swerveDrive.drive(new Translation2d(LeftX, -LeftY), rot, false, false);
     }
 
     public void close() {
-        for (int i = 0; i < 8; i++) {
-            motors[i].set(0);
-            motors[i].close();
-        }
     }
     public static RobotContainer getInstance() {
         if (instance == null) {
